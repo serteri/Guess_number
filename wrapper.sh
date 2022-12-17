57e273f75575863ca8686a7cf39853eb0e7c81c0
serteri <serteri@hotmail.com>
2022-12-17 13:39:19 +1000

    Bash script writing

--- wrapper.sh
+++ wrapper.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -u
 
-python3  ./src/game.py
+if [[ "$(python3 -V)" =~ "Python 3" ]]
+then
+  echo "Python 3 installed"
+else
+ python3 -m ensurepip --default-pip 
+
+fi
+pip install -r requirements.txt
